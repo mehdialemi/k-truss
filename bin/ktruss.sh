ebf7d67b961953231b51190a360f842ed22c13a8
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-15 18:33:06 +0330

    change run script

--- bin/ktruss.sh
+++ bin/ktruss.sh
@@ -9,20 +9,20 @@ function ktruss {
     bin/run-spark.sh "ir.ac.sbu.sbm.KTruss" 120 $input $k $p $pm 0
 }
 
-ktruss  youtube 4   20  2
-ktruss  youtube 19  20  2
-ktruss  cit-patents 4   34  2
-ktruss  cit-patents 36   34  2
-ktruss  soc-liveJournal 4 100   3
-ktruss  soc-liveJournal 40 100   3
-ktruss  soc-liveJournal 80 100   3
-ktruss  soc-liveJournal 160 100   3
-ktruss  soc-liveJournal 362 100   3
-ktruss  orkut   4   160 3
-ktruss  orkut   40   160 3
-ktruss  orkut   78   160 3
-ktruss  friendster  4   492     3
-ktruss  friendster  39   492    3
+ktruss  youtube 4   20  1
+ktruss  youtube 19  20  1
+ktruss  cit-patents 4   34  1
+ktruss  cit-patents 36   34  1
+ktruss  soc-liveJournal 4 100   1
+ktruss  soc-liveJournal 40 100   1
+ktruss  soc-liveJournal 80 100   1
+ktruss  soc-liveJournal 160 100   1
+ktruss  soc-liveJournal 362 100   1
+ktruss  orkut   4   160 2
+ktruss  orkut   40   160 2
+ktruss  orkut   78   160 2
+ktruss  friendster  4   492     2
+ktruss  friendster  39   492    2
 ktruss  twitter 1998    1157    6
 ktruss  twitter 4    1157   6
 ktruss  twitter 40    1157  6
