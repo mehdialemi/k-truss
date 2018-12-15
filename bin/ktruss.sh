67172fa7ee7d3684b02d0fc65a6916dc548bd902
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-16 01:08:03 +0330

    h optimize

--- bin/ktruss.sh
+++ bin/ktruss.sh
@@ -10,20 +10,20 @@ function ktruss {
 }
 
 ktruss  youtube 4   10  20
-ktruss  youtube 19  10  20
+#ktruss  youtube 19  10  20
 ktruss  cit-patents 4   10  68
-ktruss  cit-patents 36   10 68
+#ktruss  cit-patents 36   10 68
 ktruss  soc-liveJournal 4 10    99
-ktruss  soc-liveJournal 40 10   99
-ktruss  soc-liveJournal 80 10   99
-ktruss  soc-liveJournal 160 10  99
-ktruss  soc-liveJournal 362 10  99
+#ktruss  soc-liveJournal 40 10   99
+#ktruss  soc-liveJournal 80 10   99
+#ktruss  soc-liveJournal 160 10  99
+#ktruss  soc-liveJournal 362 10  99
 ktruss  orkut   4   10  212
-ktruss  orkut   39   10 212
-ktruss  orkut   78   10  212
+#ktruss  orkut   39   10 212
+#ktruss  orkut   78   10  212
 ktruss  friendster  4   10  1476
-ktruss  friendster  39   10 1476
+#ktruss  friendster  39   10 1476
 ktruss  twitter 1998    10  9256
-ktruss  twitter 4    10 9256
-ktruss  twitter 40    10    9256
-ktruss  twitter 80    10    9256
+#ktruss  twitter 4    10 9256
+#ktruss  twitter 40    10    9256
+#ktruss  twitter 80    10    9256
--- bin/run-spark.sh
+++ bin/run-spark.sh
@@ -7,11 +7,12 @@ master=spark://alemi-1:7077
 jar="$PWD/target/k-truss-1.0-jar-with-dependencies.jar"
 input="hdfs://alemi-1/graph-data/$3"
 k=$4
-partitions=$5
-kCoreIterations=$6
+k_plus=$5
+partitions=$6
+kCoreIterations=$7
 
 file_name=`date +%Y-%m-%d.%H.%M.%S`
 log_file="$logs/$class/$input/$file_name.log"
 
-$SPARK_HOME/bin/spark-submit --class $class --total-executor-cores $cores --master $master $jar $input $k $cores $partitions $kCoreIterations
+$SPARK_HOME/bin/spark-submit --class $class --total-executor-cores $cores --master $master $jar $input $k $k_plus $cores $partitions $kCoreIterations
 echo "Duration = $SECONDS"
