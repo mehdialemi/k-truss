11bc9e2bab34fb5f77488ab57ec1ae11fe4cf2e7
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-19 05:19:35 +0330

    optimize cogroup

--- src/main/java/ir/ac/sbu/sbm/Triangle.java
+++ src/main/java/ir/ac/sbu/sbm/Triangle.java
@@ -1,6 +1,5 @@
 package ir.ac.sbu.sbm;
 
-import it.unimi.dsi.fastutil.bytes.ByteArrayList;
 import it.unimi.dsi.fastutil.bytes.ByteList;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
@@ -9,6 +8,7 @@ import org.apache.spark.storage.StorageLevel;
 import scala.Tuple2;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Triangle {
 
@@ -18,8 +18,8 @@ public class Triangle {
     public static final byte U_SIGN = (byte) 2;
 
     public static JavaPairRDD <Edge, int[]> createTSet(JavaPairRDD <Integer, int[]> neighbors, int numPartitions) {
-        JavaPairRDD <Integer, int[]> fonl = fonl(neighbors, numPartitions);
-        JavaPairRDD <Integer, int[]> candidates = fonl.filter(t -> t._2.length > 2)
+        JavaPairRDD <Integer, int[]> fonl = fonl(neighbors);
+        JavaPairRDD <Integer, Iterable <int[]>> candidates = fonl.filter(t -> t._2.length > 2)
                 .flatMapToPair(t -> {
 
                     int size = t._2.length - 1; // one is for the first index holding node's degree
@@ -40,103 +40,113 @@ public class Triangle {
                     }
 
                     return output.iterator();
-                }).repartition(numPartitions);
+                }).groupByKey();
 
         return fonl.cogroup(candidates)
                 .flatMapToPair(t -> {
-                    Map <Edge, Tuple2 <IntList, ByteList>> map = new HashMap <>();
+                    Map<Edge, IntList> wMap = new HashMap <>();
+                    Map<Edge, IntList> vMap = new HashMap <>();
+                    Map<Edge, IntList> uMap = new HashMap <>();
+
                     int[] fVal = t._2._1.iterator().next();
                     Arrays.sort(fVal, 1, fVal.length);
                     int v = t._1;
-                    for (int[] cVal : t._2._2) {
-                        int u = cVal[0];
-                        Edge uv = new Edge(u, v);
 
-                        // The intersection determines triangles which u and vertex are two of their vertices.
-                        // Always generate and edge (u, vertex) such that u < vertex.
-                        int fi = 1;
-                        int ci = 1;
-                        while (fi < fVal.length && ci < cVal.length) {
-                            if (fVal[fi] < cVal[ci])
-                                fi++;
-                            else if (fVal[fi] > cVal[ci])
-                                ci++;
-                            else {
-                                int w = fVal[fi];
-                                Edge uw = new Edge(u, w);
-                                Edge vw = new Edge(v, w);
-                                Tuple2 <IntList, ByteList> tuple;
+                    for (Iterable <int[]> cValIter : t._2._2) {
+                        for (int[] cVal : cValIter) {
 
-                                tuple = map.computeIfAbsent(uv, k -> new Tuple2 <>(new IntArrayList(), new ByteArrayList()));
-                                tuple._1.add(w);
-                                tuple._2.add(W_SIGN);
+                            int u = cVal[0];
+                            Edge uv = new Edge(u, v);
 
-                                tuple = map.computeIfAbsent(uw, k -> new Tuple2 <>(new IntArrayList(), new ByteArrayList()));
-                                tuple._1.add(v);
-                                tuple._2.add(V_SIGN);
+                            // The intersection determines triangles which u and vertex are two of their vertices.
+                            // Always generate and edge (u, vertex) such that u < vertex.
+                            int fi = 1;
+                            int ci = 1;
+                            while (fi < fVal.length && ci < cVal.length) {
+                                if (fVal[fi] < cVal[ci])
+                                    fi++;
+                                else if (fVal[fi] > cVal[ci])
+                                    ci++;
+                                else {
+                                    int w = fVal[fi];
+                                    Edge uw = new Edge(u, w);
+                                    Edge vw = new Edge(v, w);
+                                    Tuple2 <IntList, ByteList> tuple;
 
-                                tuple = map.computeIfAbsent(vw, k -> new Tuple2 <>(new IntArrayList(), new ByteArrayList()));
-                                tuple._1.add(u);
-                                tuple._2.add(U_SIGN);
+                                    wMap.computeIfAbsent(uv, k -> new IntArrayList(cVal.length)).add(w);
+                                    vMap.computeIfAbsent(uw, k -> new IntArrayList(1)).add(v);
+                                    uMap.computeIfAbsent(vw, k -> new IntArrayList(1)).add(u);
 
-                                fi++;
-                                ci++;
+                                    fi++;
+                                    ci++;
+                                }
                             }
                         }
                     }
 
-                    Iterator <Tuple2 <Edge, Tuple2 <int[], byte[]>>> result = map.entrySet()
+                    Stream <Tuple2 <Edge, Tuple2 <Byte, int[]>>> wStream = wMap.entrySet()
                             .stream()
-                            .map(entry -> new Tuple2 <>(
-                                    entry.getKey(),
-                                    new Tuple2 <>(entry.getValue()._1.toIntArray(), entry.getValue()._2.toByteArray())))
-                            .iterator();
-                    return result;
+                            .map(entry -> new Tuple2 <>(entry.getKey(), new Tuple2 <>(W_SIGN, entry.getValue().toIntArray())));
+
+                    Stream <Tuple2 <Edge, Tuple2 <Byte, int[]>>> vStream = vMap.entrySet()
+                            .stream()
+                            .map(entry -> new Tuple2 <>(entry.getKey(), new Tuple2 <>(V_SIGN, entry.getValue().toIntArray())));
+
+                    Stream <Tuple2 <Edge, Tuple2 <Byte, int[]>>> vwStream = Stream.concat(wStream, vStream);
+
+                    Stream <Tuple2 <Edge, Tuple2 <Byte, int[]>>> uStream = uMap.entrySet()
+                            .stream()
+                            .map(entry -> new Tuple2 <>(entry.getKey(), new Tuple2 <>(U_SIGN, entry.getValue().toIntArray())));
+
+                    return Stream.concat(vwStream, uStream).iterator();
                 })
                 .groupByKey()
                 .mapValues(values -> {
-                    IntList wList = new IntArrayList();
-                    IntList vList = new IntArrayList();
-                    IntList uList = new IntArrayList();
+                    int wSize = 0;
+                    int vSize = 0;
+                    int uSize = 0;
 
-                    for (Tuple2 <int[], byte[]> value : values) {
-                        int[] vertices = value._1;
-                        byte[] signs = value._2;
-                        for (int i = 0; i < vertices.length; i++) {
-                            switch (signs[i]) {
-                                case W_SIGN:
-                                    wList.add(vertices[i]);
-                                    break;
-                                case V_SIGN:
-                                    vList.add(vertices[i]);
-                                    break;
-                                case U_SIGN:
-                                    uList.add(vertices[i]);
-                                    break;
-                            }
+                    for (Tuple2 <Byte, int[]> value : values) {
+                        switch (value._1) {
+                            case W_SIGN: wSize += value._2.length; break;
+                            case V_SIGN: vSize += value._2.length; break;
+                            case U_SIGN: uSize += value._2.length; break;
                         }
                     }
-                    int offsetW = META_LEN;
-                    int offsetV = wList.size() + META_LEN;
-                    int offsetU = wList.size() + vList.size() + META_LEN;
-                    int len = wList.size() + vList.size() + uList.size();
-                    int[] set = new int[META_LEN + len];
-                    set[0] = len;  // support of edge
-                    set[1] = offsetW + wList.size();  // exclusive max offset of w
-                    set[2] = offsetV + vList.size();  // exclusive max offset of vertex
-                    set[3] = offsetU + uList.size();  // exclusive max offset of u
 
-                    System.arraycopy(wList.toIntArray(), 0, set, offsetW, wList.size());
-                    System.arraycopy(vList.toIntArray(), 0, set, offsetV, vList.size());
-                    System.arraycopy(uList.toIntArray(), 0, set, offsetU, uList.size());
+                    int sup = wSize + vSize + uSize;
+                    int[] result = new int[META_LEN + sup];
+                    int wOffset = META_LEN;
+                    int vOffset = wOffset + wSize;
+                    int uOffset = vOffset + vSize;
+                    result[0] = sup;
+                    result[1] = vOffset;
+                    result[2] = uOffset;
+                    result[3] = result.length;
 
-                    return set;
+                    for (Tuple2 <Byte, int[]> value : values) {
+                        switch (value._1) {
+                            case W_SIGN:
+                                System.arraycopy(value._2, 0, result, wOffset, value._2.length);
+                                wOffset += value._2.length;
+                                break;
+                            case V_SIGN:
+                                System.arraycopy(value._2, 0, result, vOffset, value._2.length);
+                                vOffset += value._2.length;
+                                break;
+                            case U_SIGN:
+                                System.arraycopy(value._2, 0, result, uOffset, value._2.length);
+                                uOffset += value._2.length;
+                                break;
+                        }
+                    }
+                    return result;
                 }).persist(StorageLevel.MEMORY_AND_DISK());
     }
 
-    private static JavaPairRDD <Integer, int[]> fonl(JavaPairRDD <Integer, int[]> neighbors, int numPartition) {
+    private static JavaPairRDD <Integer, int[]> fonl(JavaPairRDD <Integer, int[]> neighbors) {
         return neighbors
-                .repartition(numPartition)
+//                .repartition(numPartition)
                 .flatMapToPair(t -> {
                     int deg = t._2.length;
                     if (deg == 0)
